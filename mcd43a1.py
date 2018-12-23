a99f7c05c10ca60faa3c6700d8ed7317b4e5f5a9
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-12-23 19:35:33 +1100

    Added convert_vrt custom function for mcd43a1.py

--- mcd43a1.py
+++ mcd43a1.py
@@ -7,10 +7,13 @@ Convert MCD43A1 HDF4 files to HDF5.
 from pathlib import Path
 from subprocess import check_call
 import tempfile
+import numpy
 import rasterio
 import h5py
 
-from wagl.hdf5 import write_h5_image, attach_attributes
+from wagl.hdf5 import write_h5_image, attach_attributes, attach_image_attributes
+from wagl.hdf5.compression import H5CompressionFilter
+from wagl.tiling import generate_tiles
 
 
 RASTERIO_PREFIX = 'tar:{}!'
@@ -130,3 +133,81 @@ def buildvrt(indir, outdir):
                 ]
 
                 check_call(cmd)
+
+
+def convert_vrt(fname, out_fname, dataset_name='dataset',
+                compression=H5CompressionFilter.LZF, filter_opts=None,
+                attrs=None):
+    """
+    Convert the VRT mosaic to HDF5.
+    The HDF5 file specified by out_fname will be opened
+    in append mode.
+    """
+    with h5py.File(out_fname) as fid:
+        with rasterio.open(fname) as rds:
+            # set default chunks and set dimensions
+            if rds.count == 3:
+                chunks = (3, 256, 256)
+                dims = (3, rds.height, rds.width)
+            else:
+                chunks = (256, 256)
+                dims = (rds.height, rds.width)
+
+            # create empty or copy the user supplied filter options
+            if not filter_opts:
+                filter_opts = dict()
+                filter_opts['chunks'] = chunks
+            else:
+                filter_opts = filter_opts.copy()
+
+
+            if 'chunks' not in filter_opts:
+                filter_opts['chunks'] = chunks
+
+            # modify to have 3D chunks if we have a multiband vrt
+            if rds.count == 3 and len(filter_opts['chunks']) != 3:
+                # copy the users original 2D chunk and insert the third
+                chunks = list(filter_opts['chunks'])
+                chunks.insert(0, 3)
+                filter_opts['chunks'] = chunks
+
+            # dataset attributes
+            if attrs:
+                attrs = attrs.copy()
+            else:
+                attrs = {}
+
+            attrs['geotransform'] = rds.transform.to_gdal()
+            attrs['crs_wkt'] = rds.crs.wkt
+
+            # dataset creation options
+            kwargs = compression.config(**filter_opts).dataset_compression_kwargs()
+            kwargs['shape'] = dims
+            kwargs['dtype'] = rds.dtypes[0]
+
+            dataset = fid.create_dataset(dataset_name, **kwargs)
+            attach_image_attributes(dataset, attrs)
+
+            # tiled processing (all cols by chunked rows)
+            ytile = filter_opts['chunks'][1] if rds.count == 3 else filter_opts['chunks'][0]
+            tiles = generate_tiles(rds.width, rds.height, rds.width, ytile)
+
+            for tile in tiles:
+                # numpy index
+                if rds.count == 3:
+                    idx = (
+                        slice(None),
+                        slice(tile[0][0], tile[0][1]),
+                        slice(tile[1][0], tile[1][1])
+                    )
+                else:
+                    idx = (
+                        slice(tile[0][0], tile[0][1]),
+                        slice(tile[1][0], tile[1][1])
+                    )
+
+                # ensure single band rds is read as 2D not 3D
+                data = rds.read(window=tile) if rds.count == 3 else rds.read(1, window=tile)
+
+                # write
+                dataset[idx] = data

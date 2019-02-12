345217bd74669408e6054b573b126c78699b386a
Passang Dorji <pd1813@vdi-n6.novalocal>
2019-02-12 17:23:26 +1100

    additional tests for for brdf_shape.py module

--- test_premodisbrdf.py
+++ test_premodisbrdf.py
@@ -6,6 +6,7 @@ test function for pre_modis_brdf computations
 import numpy as np
 import unittest 
 import pre_modis_brdf as premod
+import brdf_shape
 
 
 def gen_test_dataset():
@@ -29,7 +30,7 @@ def gen_test_dataset():
     return data_band, data_qual    
 
 
-class BrdfModuleTest(unittest.TestCase): 
+class PreModisBrdfTest(unittest.TestCase):
     
     def test_generate_tile_spatial_stats(self): 
         """ test to checks if spatial statistics of an array are computed correctly """
@@ -137,6 +138,45 @@ class BrdfModuleTest(unittest.TestCase):
         self.assertEqual(monthly_mean[1]['iso_clean_mean'][1, 1], 8.0)
         self.assertEqual(daily_mean[1]['iso_clean_mean'][1, 0], 3.75)
         self.assertEqual(yearly_mean[2011]['iso_clean_mean'][1, 1], 7.6)
+
+    def test_constants(self):
+        """ test to check if constants are not changed"""
+        constants = brdf_shape.CONSTANTS
+
+        self.assertEqual(0.015683596, constants['c11'])
+        self.assertEqual(0.055165295, constants['c12'])
+        self.assertEqual(0.371423479, constants['c22'])
+        self.assertEqual(0.189184, constants['gvol'])
+        self.assertEqual(-1.377622, constants['ggeo'])
+        self.assertEqual(0.037839423386594996, constants['ra'])
+        self.assertEqual(-0.07706873100489496, constants['rb'])
+        self.assertEqual(0.1957082863758389, constants['rc'])
+
+    def test_get_rms_indices(self):
+        """ tests computation of rms indices"""
+        alpha1 = alpha2 = np.array([1, 2, 3, 4, 5])
+        rms_val = np.sqrt((0.015683596 * alpha1**2) + (2 * 0.055165295 * alpha1 * alpha2) + (0.371423479 * alpha2**2))
+        rms = brdf_shape.get_rms_indices(alpha1, alpha2)
+        self.assertEqual(rms_val.all(), rms.all())
+
+    def test_get_afx_indices(self):
+        """ tests computation of afx indices"""
+        alpha1 = alpha2 = np.array([1, 2, 3, 4, 5])
+        afx_val = 1 + 0.189184 * alpha1 + (-1.377622 * alpha2)
+        afx = brdf_shape.get_afx_indices(alpha1, alpha2)
+        self.assertEqual(afx_val.all(), afx.all())
+
+    def test_shape_param(self):
+        """ tests computation of brdf shape function using lognormal model"""
+        fiso = np.ones((3, 5, 5), dtype='int16') * 100
+        fvol = np.ones((3, 5, 5), dtype='int16') * 50
+        fgeo = np.ones((3, 5, 5), dtype='int16') * 10
+        cov_iso = np.std(fiso, axis=0)/np.mean(fiso, axis=0)
+
+        alpha1, alpha2 = brdf_shape.get_mean_shape_param(fiso, fvol, fgeo, cov_iso)
+
+        self.assertEqual(alpha1.all(), (np.ones((5, 5), dtype='float32') * 0.5).all())
+        self.assertEqual(alpha2.all(), (np.ones((5, 5), dtype='float32') * 0.1).all())
 
 
 if __name__ == '__main__':

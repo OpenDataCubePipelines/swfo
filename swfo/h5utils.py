066aecad04daed2535bea91dde88f630d981bf74
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2019-05-07 16:14:01 +1000

    rename variable h5_container to h5_group for consistency

--- swfo/h5utils.py
+++ swfo/h5utils.py
@@ -10,7 +10,7 @@ VLEN_STRING = h5py.special_dtype(vlen=str)
 yaml_dumper = partial(yaml.dump, indent=4, default_flow_style=False)
 
 
-def _get_next_md_id(h5_container: h5py.Group, group_prefix: str) -> int:
+def _get_next_md_id(h5_group: h5py.Group, group_prefix: str) -> int:
     """
     Returns the next incremental ID for metadata versioning
     """
@@ -18,7 +18,7 @@ def _get_next_md_id(h5_container: h5py.Group, group_prefix: str) -> int:
     def _append_id(h5_key):
         ids.append(int(h5_key[1:]))
 
-    group = h5_container.get(group_prefix)
+    group = h5_group.get(group_prefix)
     if group:
         group.visit(_append_id)
 
@@ -26,7 +26,7 @@ def _get_next_md_id(h5_container: h5py.Group, group_prefix: str) -> int:
 
 
 def write_h5_md(
-        h5_container: h5py.Group,
+        h5_group: h5py.Group,
         datasets: Union[List[Dict], Dict],
         dataset_names: Optional[List[str]]=None) -> None:
     """
@@ -43,13 +43,13 @@ def write_h5_md(
     path_fmt = '/metadata/{}'
 
     def _write_dataset(
-            h5_container: h5py.Group,
+            h5_group: h5py.Group,
             path: str,
             dataset: Dict) -> h5py.Dataset:
         """
         Internal function for writing dataset metadata to a H5 file
         """
-        ds = h5_container.create_dataset(
+        ds = h5_group.create_dataset(
             path,
             dtype=VLEN_STRING,
             shape=(1,),
@@ -57,17 +57,17 @@ def write_h5_md(
         ds[()] = yaml_dumper(dataset)
 
         current_path = path.rsplit('/', 1)[0] + '/current'
-        if h5_container.get(current_path):
-            del h5_container[current_path]
+        if h5_group.get(current_path):
+            del h5_group[current_path]
 
-        h5_container[current_path] = h5py.SoftLink(ds.name)
+        h5_group[current_path] = h5py.SoftLink(ds.name)
 
-        return h5_container[current_path]
+        return h5_group[current_path]
 
     if not dataset_names:
-        doc_id = _get_next_md_id(h5_container, path_fmt.format(''))
+        doc_id = _get_next_md_id(h5_group, path_fmt.format(''))
         _write_dataset(
-            h5_container,
+            h5_group,
             path_fmt.format('_' + str(doc_id)),
             datasets
         )
@@ -78,16 +78,16 @@ def write_h5_md(
         )
         for idx, dn in enumerate(dataset_names):
             dataset_path = path_fmt.format(dn)
-            doc_id = _get_next_md_id(h5_container, dataset_path)
+            doc_id = _get_next_md_id(h5_group, dataset_path)
             current_ref = _write_dataset(
-                h5_container,
+                h5_group,
                 dataset_path + '/_' + str(doc_id),
                 datasets[idx]
             )
             collection_layout[idx, :] = h5py.VirtualSource(current_ref)
 
         collection_path = path_fmt.format('current')
-        if h5_container.get(collection_path):
-            del h5_container[collection_path]
+        if h5_group.get(collection_path):
+            del h5_group[collection_path]
 
-        h5_container.create_virtual_dataset(collection_path, collection_layout)
+        h5_group.create_virtual_dataset(collection_path, collection_layout)
